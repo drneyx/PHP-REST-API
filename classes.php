aa9a232b24c7f24350626f13194f6b3d05a8871a
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-19 16:22:14 +0300

    Add Controller

--- classes.php
+++ classes.php
@@ -2,6 +2,6 @@
 require_once __DIR__."/Database/db.php";
 require_once __DIR__."/Database/query.php";
 require_once __DIR__."/Modal/factory.php";
-require_once __DIR__."/Modal/DVD.php";
-require_once __DIR__."/Modal/Book.php";
-require_once __DIR__."/Modal/furniture.php";
+require_once __DIR__."/Controller/DVD.php";
+require_once __DIR__."/Controller/Book.php";
+require_once __DIR__."/Controller/furniture.php";
