f71cbafd1511403fa128a37dc06d85a625d73385
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-24 17:01:24 +0300

    Fix bug

--- products.php
+++ products.php
@@ -1,42 +1,31 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header('Access-Control-Allow-Headers: X-Requested-With');
+header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
 header('Content-Type: application/json');
 
-
-// Include action.php file
-// include_once 'db.php';
 require_once __DIR__."/Shared/utils.php";
-// Create object of Users class
-// $user = new Database();
+
 $util = new Util();
-// create a api variable to get HTTP method dynamically
 $api = $_SERVER['REQUEST_METHOD'];
 
 
 function parseInput()
 {
     $data = file_get_contents("php://input");
-
     if($data == false)
         return array();
-
     parse_str($data, $result);
-
     return $result;
 }
-
-// get id from url
-$id = intval($_GET['id'] ?? '');
 
 if ($api == 'GET') {
     echo $util->getAllProducts();
 }
 
 if ($api == 'POST') {
-    $data = json_encode($_POST);
-    echo $util->addProduct($data);
+    $body = file_get_contents("php://input");
+    echo $util->addProduct($body);
 }
 
 if ($api == 'DELETE') {
